300a9fccd754fc3b15f50b560b6e6ad455577697
OsamaFarooqi <40718347+OsamaFarooqi@users.noreply.github.com>
2025-11-17 17:39:51 +0500

    -Delete note usin API Call is added

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -6,9 +6,9 @@ const NoteState = (props) => {
   const initialNotes = [];
   const [notes, setNotes] = useState(initialNotes);
 
+  // Get All Notes from Database
   const getAllNotes = async () => {
     // API Call - fetch all Notes:
-    let allNotes = [];
     try {
       const response = await fetch(`${host}/api/notes/fetchallnotes`, {
         method: "GET",
@@ -30,6 +30,7 @@ const NoteState = (props) => {
     }
   };
 
+  // Add a New Note
   const addNote = async (title, description, tag) => {
     // API Call - Add a Note:
     try {
@@ -60,13 +61,32 @@ const NoteState = (props) => {
     }
   };
 
-  // Delete a note
-  const deleteNote = (id) => {
-    console.log(id);
-    const newNotes = notes.filter((note) => note._id !== id);
-    setNotes(newNotes);
+  // Delete an Existing Note
+  const deleteNote = async (id) => {
+    // API Call - Add a Note:
+    try {
+      console.log(id);
+      const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token":
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjkxNDZmYzY1YmFkNzg3ODA3NzkxYzBjIn0sImlhdCI6MTc2Mjk0NzMxNX0.CEfU8r8YlOSaYiK39J2qFG4aAcmmdOVAQz_UvPq8gFk",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const result = await response.json();
+      console.log(result);
+      // Client Side Logic - fetch all Notes:
+      getAllNotes(); // to get latest notes from server. To keep single copy of notes displayed and stored(self-coded).
+    } catch (error) {
+      console.error(error.message);
+    }
   };
-  // Edit a note
+
+  // Edit an Existing Note
   const editNote = (id) => {
     console.log(id);
   };
